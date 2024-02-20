d70e2d355f4b21a5f0c5e3addf05dc2093e01e56
JaeHyeok29 <jbs1896@gmail.com>
2024-02-20 11:09:01 +0900

    df

--- student/src/main/java/com/nhnacademy/edu/springboot/student/StudentApplication.java
+++ student/src/main/java/com/nhnacademy/edu/springboot/student/StudentApplication.java
@@ -2,22 +2,13 @@ package com.nhnacademy.edu.springboot.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication //spring-boot 실행의 시작점
-public class StudentApplication extends SpringBootServletInitializer {
-
+@SpringBootApplication(exclude= RedisAutoConfiguration.class)
+public class StudentApplication {
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(StudentApplication.class);
-		application.run(args);
-		// spring-boot 시작 명령.
-		// args를 넣지않으면 외부에서 넣는 인자가 넘어오지 않는 문제 발생할 수 있음.
 		SpringApplication.run(StudentApplication.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(StudentApplication.class);
-	}
-
 }
